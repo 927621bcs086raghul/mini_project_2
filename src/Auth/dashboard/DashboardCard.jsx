928398cf19bb2c36756936a2c6d3f0c40be4ecf7
Raghul R <raghulajay814@gmail.com>
2025-10-09 18:19:30 +0530

    in CRUD userupdate completed on both table and card view pending delete user only

--- src/Auth/dashboard/DashboardCard.jsx
+++ src/Auth/dashboard/DashboardCard.jsx
@@ -1,17 +1,25 @@
 import { Avatar, Card, Flex, Pagination,Button } from "antd";
 const { Meta } = Card;
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserdataReq,modalOperatorOpen } from '../../redux/utils';
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import "./DashboardCard.css";
 const DashboardCard = () => {
   const { allUserLoading, AllUser, total } = useSelector((state) => state.auth);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
-
+  const dispatch=useDispatch();
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentUsers = AllUser.slice(startIndex, endIndex);
+  console.log(currentUsers)
+const handleEdit=(id)=>{
+console.log(id);
+ dispatch(getUserdataReq(id));
+  dispatch(modalOperatorOpen({option:'edit',id:id}));
+
+}
   return (
     <div>
       <div className="dashboard-card">
@@ -40,6 +48,7 @@ const DashboardCard = () => {
                   shape="circle"
                   icon={<EditOutlined />}
                   className="hover-button"
+                  onClick={()=>handleEdit(item.id)}
                 />
                 <Button
                   type="primary"
